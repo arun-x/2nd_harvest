a19f33a874e1f1461cb25267a9b81a5bfc4fc7c9
arun-x <arun192204@gmail.com>
2026-08-10 18:01:11 +0530

    Update BaseModel helpers

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -1,1 +1,10 @@
+<?php
+require_once __DIR__ . '/../Core/Database.php';
 
+abstract class BaseModel
+{
+    protected static function db(): PDO
+    {
+        return Database::connection();
+    }
+}
